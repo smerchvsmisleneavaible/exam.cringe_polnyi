0332d6b4d7bd4f46c2e821d716f049043f33bbc2
smerchvsmisleneavaible <102783865+smerchvsmisleneavaible@users.noreply.github.com>
2024-04-10 15:32:13 +0700

    Add files via upload

---
+++ exam(1).sql
@@ -0,0 +1,311 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 10 2024 г., 11:30
+-- Версия сервера: 8.0.30
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `catalog`
+--
+
+CREATE TABLE `catalog` (
+  `id` int NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `price` decimal(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `catalog`
+--
+
+INSERT INTO `catalog` (`id`, `name`, `price`) VALUES
+(1, 'Деталь', '22222.00'),
+(2, 'Кусок пластика', '22222.00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order`
+--
+
+CREATE TABLE `order` (
+  `id` int NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_end` date DEFAULT NULL,
+  `equip` varchar(32) NOT NULL,
+  `type_break` varchar(32) NOT NULL,
+  `client_phone` varchar(32) NOT NULL,
+  `state_id` int NOT NULL DEFAULT '3',
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `order`
+--
+
+INSERT INTO `order` (`id`, `date_add`, `date_end`, `equip`, `type_break`, `client_phone`, `state_id`, `user_id`) VALUES
+(1, '2024-04-08 21:00:00', '2024-04-17', 'мяу', 'мяуeeeeeeeee', '89074445533', 1, 2),
+(3, '2024-04-09 21:00:00', '2024-04-18', 'Параша', 'Оторвалось что-то', '89996667543', 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `id` int NOT NULL,
+  `order_id` int NOT NULL,
+  `catalog_id` int NOT NULL,
+  `date_order` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `order_detail`
+--
+
+INSERT INTO `order_detail` (`id`, `order_id`, `catalog_id`, `date_order`) VALUES
+(1, 1, 1, '2024-04-09 08:38:41'),
+(2, 1, 1, '2024-04-08 21:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `report`
+--
+
+CREATE TABLE `report` (
+  `id` int NOT NULL,
+  `time` int NOT NULL,
+  `user_id` int NOT NULL,
+  `rate` int NOT NULL DEFAULT '5',
+  `order_id` int NOT NULL,
+  `price` decimal(8,2) DEFAULT NULL,
+  `help` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `report`
+--
+
+INSERT INTO `report` (`id`, `time`, `user_id`, `rate`, `order_id`, `price`, `help`) VALUES
+(3, 2, 2, 4, 1, '233.00', 'Всё починили)'),
+(5, 2, 3, 2, 3, '2222.00', 'Ну мы всё сломали)))');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `role`
+--
+
+CREATE TABLE `role` (
+  `id` int NOT NULL,
+  `role` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `role`
+--
+
+INSERT INTO `role` (`id`, `role`) VALUES
+(1, 'Менеджер'),
+(2, 'Исполнитель');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `state`
+--
+
+CREATE TABLE `state` (
+  `id` int NOT NULL,
+  `state` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `state`
+--
+
+INSERT INTO `state` (`id`, `state`) VALUES
+(1, 'в работе'),
+(2, 'выполнено'),
+(3, 'в ожидании');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `fio` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `fio`, `username`, `password`, `role_id`) VALUES
+(1, 'Тестер Тестировщик Тестович', 'tester', '123456', 1),
+(2, 'Такими Темпами Я', 'Soidu_S_Uma', '123456', 2),
+(3, 'А Чё Я', 'ToSrazu', '876543', 2);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `catalog`
+--
+ALTER TABLE `catalog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`user_id`),
+  ADD KEY `state_id` (`state_id`);
+
+--
+-- Индексы таблицы `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_order` (`order_id`),
+  ADD KEY `id_detail` (`catalog_id`),
+  ADD KEY `id_detail_2` (`catalog_id`);
+
+--
+-- Индексы таблицы `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `time_calc` (`time`,`user_id`,`order_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `catalog`
+--
+ALTER TABLE `catalog`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `report`
+--
+ALTER TABLE `report`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_ibfk_4` FOREIGN KEY (`state_id`) REFERENCES `state` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD CONSTRAINT `order_detail_ibfk_1` FOREIGN KEY (`catalog_id`) REFERENCES `catalog` (`id`),
+  ADD CONSTRAINT `order_detail_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
